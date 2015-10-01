f8367615854762f586b144eec8c7110dade9615a
Greg DeLozier <gregdelozier@gmail.com>
2015-09-30 22:45:33 -0400

    lecture_4

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -57,6 +57,4 @@ class NewVisitorTest(unittest.TestCase):
         # Satisfied, she goes back to sleep
 
 if __name__ == '__main__':
-    unittest.main(warnings='ignore')
-
-
+    unittest.main(warnings='ignore')    
